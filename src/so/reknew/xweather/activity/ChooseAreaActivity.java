653550d884525e577a90de1c041e61f8178bce9e
HideIt <chunmaoliao@gmail.com>
2016-03-05 13:55:35 +0800

    时差~

--- src/so/reknew/xweather/activity/ChooseAreaActivity.java
+++ src/so/reknew/xweather/activity/ChooseAreaActivity.java
@@ -3,9 +3,11 @@ package so.reknew.xweather.activity;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.R;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -37,6 +39,9 @@ public class ChooseAreaActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.choose_area);
+		listView =(ListView)findViewById(R.id.list_view);
 	}
 }
